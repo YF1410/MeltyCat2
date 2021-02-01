79ee08be8150d544d1f6581421ab555fd4769973
YutaFujinami <k020g1410@g.neec.ac.jp>
2021-02-01 16:49:56 +0900

    stagemap追加

---
+++ MeltyCat2/StageMap.h
@@ -0,0 +1,9 @@
+#pragma once
+
+extern int stage1[12][15];
+extern int stage2[12][15];
+extern int stage3[12][15];
+extern int stage4[12][15];
+extern int stage5[12][15];
+extern int stage6[12][15];
+extern int stage7[12][15];
